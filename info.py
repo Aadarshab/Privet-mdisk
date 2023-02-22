78e0457cc19d9cc9ebd3c625b0e680564ef3884e
Aadarshab <72687881+Aadarshab@users.noreply.github.com>
2023-02-22 15:35:25 +0530

    Update info.py

--- info.py
+++ info.py
@@ -11,19 +11,19 @@ def is_enabled(value, default):
         return default
 
 # Bot information
-SESSION = environ.get("SESSION", "")
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+SESSION = environ.get("SESSION", "Mdisk-Search-bot")
+API_ID = int(environ.get("API_ID", "24236993"))
+API_HASH = environ.get("API_HASH", "0a56df2f25eefcace1c2e8948106dd66")
+BOT_TOKEN = environ.get("BOT_TOKEN", "5886084142:AAFNTg0tF69rTt89WZlEoamKeo57DXK1d9A")
 # Bot settings
 CACHE_TIME = int(environ.get("CACHE_TIME", 300))
 USE_CAPTION_FILTER = bool(environ.get("USE_CAPTION_FILTER", False))
 PICS = (environ.get("PICS", "https://telegra.ph/file/7e56d907542396289fee4.jpg https://telegra.ph/file/9aa8dd372f4739fe02d85.jpg https://telegra.ph/file/adffc5ce502f5578e2806.jpg https://telegra.ph/file/6937b60bc2617597b92fd.jpg https://telegra.ph/file/09a7abaab340143f9c7e7.jpg https://telegra.ph/file/5a82c4a59bd04d415af1c.jpg https://telegra.ph/file/323986d3bd9c4c1b3cb26.jpg https://telegra.ph/file/b8a82dcb89fb296f92ca0.jpg https://telegra.ph/file/31adab039a85ed88e22b0.jpg https://telegra.ph/file/c0e0f4c3ed53ac8438f34.jpg https://telegra.ph/file/eede835fb3c37e07c9cee.jpg https://telegra.ph/file/e17d2d068f71a9867d554.jpg https://telegra.ph/file/8fb1ae7d995e8735a7c25.jpg https://telegra.ph/file/8fed19586b4aa019ec215.jpg https://telegra.ph/file/8e6c923abd6139083e1de.jpg https://telegra.ph/file/0049d801d29e83d68b001.jpg")).split()
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get("ADMINS", "").split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get("CHANNELS", "").split()]
-auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get("AUTH_USERS", "").split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get("ADMINS", "1324432518").split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get("CHANNELS", "-1001720244952").split()]
+auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get("AUTH_USERS", "1324432518").split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_channel = environ.get("AUTH_CHANNEL")
 auth_grp = environ.get("AUTH_GROUP")
@@ -31,8 +31,8 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get("DATABASE_URI", "")
-DATABASE_NAME = environ.get("DATABASE_NAME", "")
+DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://Movie:AB@cluster0.g1lthiw.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get("DATABASE_NAME", "Mdisk-Search-bot")
 COLLECTION_NAME = environ.get("COLLECTION_NAME", "Telegram_files")
 
 # Others
